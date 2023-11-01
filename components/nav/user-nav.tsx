49e284a163a5f31fcb9811111be296588f27d18a
Scott Weaver <scott@weaverscott.com>
2023-11-01 13:08:23 -0500

    Clicking name takes you to your profile

--- components/nav/user-nav.tsx
+++ components/nav/user-nav.tsx
@@ -1,39 +1,39 @@
-"use client";
+"use client"
 
-import NotificationToggle from "@/components/nav/notificationtoggle";
-import UserNavLinks from "@/components/nav/user-nav-links";
-import { StreakDisplay } from "@/components/streaks/streak-display";
-import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
+import NotificationToggle from "@/components/nav/notificationtoggle"
+import UserNavLinks from "@/components/nav/user-nav-links"
+import { StreakDisplay } from "@/components/streaks/streak-display"
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
 import {
   Sheet,
   SheetContent,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-} from "@/components/ui/sheet";
-import { ThemeToggle } from "@/components/ui/theme-toggle";
-import UserAvatar from "@/components/user-avatar";
-import { useNav } from "@/hooks/useNav";
-import { registerServiceWorker } from "@/lib/notifications";
-import { useUser } from "@clerk/nextjs";
-import Link from "next/link";
-import { useEffect } from "react";
+} from "@/components/ui/sheet"
+import { ThemeToggle } from "@/components/ui/theme-toggle"
+import UserAvatar from "@/components/user-avatar"
+import { useNav } from "@/hooks/useNav"
+import { registerServiceWorker } from "@/lib/notifications"
+import { useUser } from "@clerk/nextjs"
+import Link from "next/link"
+import { useEffect } from "react"
 
 export function UserNav() {
-  const { open, setOpen } = useNav();
-  const { user } = useUser();
+  const { open, setOpen } = useNav()
+  const { user } = useUser()
 
   useEffect(() => {
     async function setUpServiceWorker() {
       try {
-        await registerServiceWorker();
+        await registerServiceWorker()
       } catch (err) {
-        console.error(err);
+        console.error(err)
       }
     }
-    setUpServiceWorker();
-  }, []);
+    setUpServiceWorker()
+  }, [])
 
   return (
     <Sheet onOpenChange={setOpen} open={open}>
@@ -49,8 +49,15 @@ export function UserNav() {
       </SheetTrigger>
       <SheetContent>
         <SheetHeader>
-          <SheetTitle className="flex flex-row items-center justify-center gap-2 p-2">
-            <UserAvatar />
+          <SheetTitle>
+            <Link
+              href={`/u/${user?.username}`}
+              prefetch={false}
+              className="flex flex-row items-center justify-center gap-1"
+            >
+              <UserAvatar />
+              {user?.username}
+            </Link>
           </SheetTitle>
         </SheetHeader>
         <Separator className="my-4" />
@@ -90,5 +97,5 @@ export function UserNav() {
         </div>
       </SheetContent>
     </Sheet>
-  );
+  )
 }
--- components/profile/profileachievements.tsx
+++ components/profile/profileachievements.tsx
@@ -1,14 +1,7 @@
-import { ProfileAchievementWithAchievement } from "@/drizzle/schema"
 import { FC } from "react"
 import Image from "next/image"
-
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip"
 import { getUserAchievements } from "@/lib/actions/profiles"
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover"
 
 interface ProfileAchievementsProps {
   userId: string
@@ -27,39 +20,37 @@ const ProfileAchievements: FC<ProfileAchievementsProps> = async ({
       <div className="mb-4 grid grid-cols-4 gap-2 md:grid-cols-8 xl:grid-cols-12">
         {achievements &&
           achievements.map(({ achievement }) => (
-            <TooltipProvider key={achievement.id}>
-              <Tooltip>
-                <TooltipTrigger>
-                  <div className="border-nuetral=800 flex h-20 w-20 flex-col items-center justify-center rounded-full border bg-neutral-100 dark:bg-neutral-900">
-                    <Image
-                      alt={achievement.name || "Achievement"}
-                      src={achievement.image || "images/alert-octagon.svg"}
-                      width={100}
-                      height={100}
-                      className="h-auto w-auto object-cover transition-all hover:scale-110"
-                    />
-                  </div>
-                  <p className="h-8 max-h-8 whitespace-pre-line text-center text-xs">
+            <Popover key={achievement.id}>
+              <PopoverTrigger>
+                <div className="border-nuetral=800 flex h-20 w-20 flex-col items-center justify-center rounded-full border bg-neutral-100 text-center dark:bg-neutral-900">
+                  <Image
+                    alt={achievement.name || "Achievement"}
+                    src={achievement.image || "images/alert-octagon.svg"}
+                    width={100}
+                    height={100}
+                    className="h-auto w-auto object-cover transition-all hover:scale-110"
+                  />
+                </div>
+                <p className="h-8 max-h-8 whitespace-pre-line text-center text-xs">
+                  {achievement.name || "Achievement"}
+                </p>
+              </PopoverTrigger>
+              <PopoverContent align="center" side="bottom">
+                <div className="flex flex-col items-center justify-center">
+                  <h3 className="text-xl font-bold">
                     {achievement.name || "Achievement"}
+                  </h3>
+                  <p className="text-center text-sm">
+                    {achievement.description || "Description"}
                   </p>
-                </TooltipTrigger>
-                <TooltipContent align="center" side="bottom">
-                  <div className="flex flex-col items-center justify-center">
-                    <h3 className="text-xl font-bold">
-                      {achievement.name || "Achievement"}
-                    </h3>
-                    <p className="text-center text-sm">
-                      {achievement.description || "Description"}
-                    </p>
-                    <p className="text-center text-sm">
-                      Awarded on:{" "}
-                      {achievement.created_at?.toLocaleDateString("en-US") ||
-                        "Date"}
-                    </p>
-                  </div>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
+                  <p className="text-center text-sm">
+                    Awarded on:{" "}
+                    {achievement.created_at?.toLocaleDateString("en-US") ||
+                      "Date"}
+                  </p>
+                </div>
+              </PopoverContent>
+            </Popover>
           ))}
       </div>
     </div>
