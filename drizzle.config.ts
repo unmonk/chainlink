6ec46f3334b693eb701d369cc2c18d4ad43d1899
Scott Weaver <Scott@WeaverScott.com>
2024-01-27 19:16:03 -0600

    clerk patch, drizzle update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,14 @@
-import dotenv from "dotenv";
-import type { Config } from "drizzle-kit";
+import dotenv from "dotenv"
+import type { Config } from "drizzle-kit"
 
-dotenv.config({ path: ".env.local" });
+dotenv.config({ path: ".env.local" })
 
 export default {
   schema: "./drizzle/schema.ts",
   driver: "mysql2",
   out: "./drizzle",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    uri: process.env.DATABASE_URL!,
   },
   breakpoints: true,
-} satisfies Config;
+} satisfies Config
