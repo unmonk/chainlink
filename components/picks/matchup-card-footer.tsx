a666d8e3f812e24a8c05c666d24f9b1dbdc6e1c4
Scott Weaver <Scott@WeaverScott.com>
2024-02-24 13:49:21 -0600

    Refactor status checks and handle new matchup statuses

--- components/picks/matchup-card-footer.tsx
+++ components/picks/matchup-card-footer.tsx
@@ -1,12 +1,12 @@
-import { Button } from "@/components/ui/button";
-import { Matchup, MatchupStatus } from "@/drizzle/schema";
-import Link from "next/link";
-import { FC } from "react";
+import { Button } from "@/components/ui/button"
+import { Matchup, MatchupStatus } from "@/drizzle/schema"
+import Link from "next/link"
+import { FC } from "react"
 
 interface MatchupCardFooterProps {
-  homePercent?: number;
-  awayPercent?: number;
-  matchup: Partial<Matchup>;
+  homePercent?: number
+  awayPercent?: number
+  matchup: Partial<Matchup>
 }
 
 const MatchupCardFooter: FC<MatchupCardFooterProps> = ({
@@ -17,17 +17,20 @@ const MatchupCardFooter: FC<MatchupCardFooterProps> = ({
   const homeWinClass =
     matchup.winner_id === matchup.home_id
       ? "overline decoration-4 decoration-blue-500 font-bold"
-      : "";
+      : ""
 
   const awayWinClass =
     matchup.winner_id === matchup.away_id
       ? "overline decoration-4 decoration-blue-500 font-bold"
-      : "";
+      : ""
   return (
     <div className="under grid grid-cols-3 ">
       <div className="col-span-1 ml-4 text-left">
         {(matchup.status === "STATUS_FINAL" ||
           matchup.status === "STATUS_IN_PROGRESS" ||
+          matchup.status === "STATUS_FIRST_HALF" ||
+          matchup.status === "STATUS_SECOND_HALF" ||
+          matchup.status === "STATUS_FULL_TIME" ||
           matchup.status === "STATUS_END_PERIOD" ||
           matchup.status === "STATUS_HALFTIME") &&
           (matchup.away_value ? (
@@ -83,6 +86,9 @@ const MatchupCardFooter: FC<MatchupCardFooterProps> = ({
       </div>
       <div className="col-span-1 mr-4 text-right">
         {(matchup.status === "STATUS_FINAL" ||
+          matchup.status === "STATUS_FIRST_HALF" ||
+          matchup.status === "STATUS_SECOND_HALF" ||
+          matchup.status === "STATUS_FULL_TIME" ||
           matchup.status === "STATUS_IN_PROGRESS" ||
           matchup.status === "STATUS_END_PERIOD" ||
           matchup.status === "STATUS_HALFTIME") &&
@@ -93,7 +99,7 @@ const MatchupCardFooter: FC<MatchupCardFooterProps> = ({
           ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default MatchupCardFooter;
+export default MatchupCardFooter
