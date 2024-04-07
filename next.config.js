18c3314db240a062fcffe45e088e57d2aa68e362
Scott Weaver <Scott@WeaverScott.com>
2024-04-07 00:44:42 -0500

    fixes?

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ["mysql2"],
+  },
   images: {
     remotePatterns: [
       {
