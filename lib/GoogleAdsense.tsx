7802de6ac5214089285e5bcb24313233c2b99403
Scott Weaver <Scott@WeaverScott.com>
2024-11-25 18:06:41 -0600

    google adsense tag

---
+++ lib/GoogleAdsense.tsx
@@ -0,0 +1,21 @@
+import Script from "next/script";
+
+type Props = {
+  pId: string;
+};
+
+const GoogleAdsense: React.FC<Props> = ({ pId }) => {
+  if (process.env.NODE_ENV !== "production") {
+    return null;
+  }
+  return (
+    <Script
+      async
+      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}`}
+      crossOrigin="anonymous"
+      strategy="afterInteractive"
+    />
+  );
+};
+
+export default GoogleAdsense;
